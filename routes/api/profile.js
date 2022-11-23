ca1628529b7d38826ce38ca8503b7552abca6036
Ken Wan <kenwan006@gmail.com>
2022-11-22 23:09:11 -0800

    Delete profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -12,7 +12,9 @@ const User = require('../../models/User');
 // @access  Private
 router.get('/me', auth, async (req, res) => {
     try {
+        //remember in the auth, token was decoded to user and save it to req.user object!!
         const profile = await Profile.findOne({ user: req.user.id}).populate("user", ['name', 'avatar']);
+        
         if(!profile) {
             return res.status(400).json({ msg: 'There is no profile for this user'});
         }
@@ -182,9 +184,30 @@ async (req, res) => {
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
-        
     }
 }
 );
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        // Get remove index
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+        profile.experience.splice(removeIndex, 1);
+
+        await profile.save();
+
+        res.json(profile);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
 
 module.exports = router;
