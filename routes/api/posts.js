27b7e4a28fcf33e0c83417655bb2da55c7937112
Ken Wan <kenwan006@gmail.com>
2022-11-26 13:28:25 -0800

    Like & Unlike post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -63,7 +63,7 @@ router.get('/:id', auth, async(req, res) => {
         res.json(post);
     } catch (err) {
         console.error(err.message);
-        if(err.kind == 'ObjectId') {
+        if(err.kind === 'ObjectId') {
             return res.status(404).json({ msg: 'Post not found'});
         }
         res.status(500).send('Server Error');
@@ -91,11 +91,61 @@ router.delete('/:id', auth, async(req, res) => {
         res.json({ msg: 'Post removed'});
     } catch (err) {
         console.error(err.message);
-        if(err.kind == 'ObjectId') {
+        if(err.kind === 'ObjectId') {
             return res.status(404).json({ msg: 'Post not found'});
         }
         res.status(500).send('Server Error');
     }
 });
 
-module.exports = router;
+// @route   PUT api/posts/like/:id
+// @desc    Like a post
+// @access  Private
+router.put('/like/:id', auth, async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      
+      // Check if the post has already been liked
+      if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+        return res.status(400).json({ msg: 'Post already liked'});
+      }
+
+      post.likes.unshift({ user: req.user.id }); // add the current user to the like list of this post
+
+      await post.save();
+
+      res.json(post.likes);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+// @route   PUT api/posts/unlike/:id
+// @desc    Unlike a post
+// @access  Private
+router.put('/unlike/:id', auth, async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      
+      // Check if the post has already been liked
+      if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+        return res.status(400).json({ msg: 'Post has not yet been liked'});
+      }
+
+      // Get remove index
+      const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+
+      post.likes.splice(removeIndex, 1);
+
+      await post.save();
+
+      res.json(post.likes);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+module.exports = router; 
