7fb38c3bc7446749ab1b30fc0f92b6565625326b
Ken Wan <kenwan006@gmail.com>
2023-01-29 14:19:29 -0800

    Alert Component & Action Call

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -2,7 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 import { SET_ALERT, REMOVE_ALERT} from './types';
 
-export const setAlert = (msg, alertType, timeout = 3000) => dispatch => {
+export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
     const id = uuidv4();
     dispatch({
         type: SET_ALERT,
@@ -11,3 +11,4 @@ export const setAlert = (msg, alertType, timeout = 3000) => dispatch => {
 
     setTimeout(() => dispatch( { type: REMOVE_ALERT, payload: id }), timeout)
 };
+ 
