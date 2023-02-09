dd869aef56a98dab6346c2ebba46f3fd2bfcc0cf
Ken Wan <kenwan006@gmail.com>
2023-02-08 23:37:35 -0800

    Profile Experience & Education Display

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -48,6 +48,21 @@ const Profile = ({ getProfileById, profile: { profile }, auth }) => {
                 <h4>No experience credentials</h4>
               )}
             </div>
+            <div className="profile-edu bg-white p-2">
+              <h2 className="text-primary">Education</h2>
+              {profile.education.length > 0 ? (
+                <Fragment>
+                  {profile.education.map((education) => (
+                    <ProfileEducation
+                      key={education._id}
+                      education={education}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No education credentials</h4>
+              )}
+            </div>
           </div>
         </Fragment>
       )}
