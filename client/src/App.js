1719692aadf00744f13cb89a9b1865b72d424640
Ken Wan <kenwan006@gmail.com>
2023-02-23 22:54:07 -0800

    new domain

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing  from './components/layout/Landing';
@@ -15,6 +15,7 @@ import Posts from './components/posts/Posts';
 import Post from './components/post/Post';
 import NotFound from './components/layout/NotFound';
 import PrivateRoute  from './components/routing/PrivateRoute';
+import { LOGOUT } from './actions/types';
 import './App.css';
 // Redux
 import { Provider } from 'react-redux';
@@ -29,43 +30,48 @@ if (localStorage.token) {
 const App = () => {
   useEffect(() => {
     store.dispatch(loadUser());
-  },[]);
+
+    // log user out from all tabs if they log out in one tab
+    window.addEventListener('storage', () => {
+      if (!localStorage.token) store.dispatch({ type: LOGOUT });
+    });
+  }, []);
 
   return (
     <Provider store={store}>
       <Router>
-          <Navbar />
-          <Alert />
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="register" element={<Register />} />
-            <Route path="login" element={<Login />} />
-            <Route path="profiles" element={<Profiles />} />
-            <Route path="profile/:id" element={<Profile />} />
-            <Route
+        <Navbar />
+        <Alert />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+          <Route path="profiles" element={<Profiles />} />
+          <Route path="profile/:id" element={<Profile />} />
+          <Route
             path="dashboard"
             element={<PrivateRoute component={Dashboard} />}
-            />
-            <Route
+          />
+          <Route
             path="create-profile"
             element={<PrivateRoute component={ProfileForm} />}
-            />
-            <Route
+          />
+          <Route
             path="edit-profile"
             element={<PrivateRoute component={ProfileForm} />}
-            />
-            <Route
+          />
+          <Route
             path="add-experience"
             element={<PrivateRoute component={AddExperience} />}
-            />
-            <Route
+          />
+          <Route
             path="add-education"
             element={<PrivateRoute component={AddEducation} />}
-            />
-            <Route path="posts" element={<PrivateRoute component={Posts} />} />
-            <Route path="posts/:id" element={<PrivateRoute component={Post} />} />
-            <Route path="/*" element={<NotFound />} />
-          </Routes>
+          />
+          <Route path="posts" element={<PrivateRoute component={Posts} />} />
+          <Route path="posts/:id" element={<PrivateRoute component={Post} />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
       </Router>
     </Provider>
   );
--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -2,14 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-const Alert = ({ alerts }) => alerts !== null && alerts.length > 0 && alerts.map(alert => (
-    <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+const Alert = ({ alerts }) => (
+  <div className="alert-wrapper">
+    {alerts.map((alert) => (
+      <div key={alert.id} className={`alert alert-${alert.alertType}`}>
         {alert.msg}
-    </div>
-));
+      </div>
+    ))}
+  </div>
+);
 
 Alert.propTypes = {
-    alerts: PropTypes.array.isRequired
+  alerts: PropTypes.array.isRequired
 };
 
 const mapStateProps = state =>({ 
